78ef19f679f3fc2abf8eeea9eb38c96b4b3df824
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-18 12:27:53 -0300

    inicio tabView

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TabViewAnimated, SceneMap } from 'react-native-tab-view';
+import TabBarMenu from './TabBarMenu';
 
-const Principal = props => (
-    <View>
-        <Text>Tela Principal</Text>
-    </View>
-)
+const Conversas = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const Contatos = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
 
-export default Principal;
+export default class Principal extends Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: 'Conversas' },
+      { key: '2', title: 'Contatos' },
+    ],
+  };
+
+  _handleChangeTab = index => this.setState({ index });
+
+  _renderHeader = props => <TabBarMenu {...props} />;
+
+  _renderScene = SceneMap({
+    '1': Conversas,
+    '2': Contatos,
+  });
+
+  render() {
+    return (
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onRequestChangeTab={this._handleChangeTab}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
