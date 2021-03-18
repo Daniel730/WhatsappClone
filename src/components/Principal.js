83c164fe45771021edd4a9d9de49214ca86088ee
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-18 10:45:55 -0300

    Autenticação com email e senha adicionados

---
+++ src/components/Principal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Principal = props => (
+    <View>
+        <Text>Tela Principal</Text>
+    </View>
+)
+
+export default Principal;
