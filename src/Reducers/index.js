be4c202716b8a05f1c84b66a2bf148a6a99735a6
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-22 15:05:30 -0300

    Implementação Nova Conversa

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import AutenticacaoReducer from './AutenticacaoReducer';
 import AppReducer from './AppReducer';
 import ListaContatosReducer from './ListaContatosReducer';
+import ListaConversaReducer from './ListaConversaReducer';
 
 export default combineReducers({
     AutenticacaoReducer,
     AppReducer,
-    ListaContatosReducer
+    ListaContatosReducer,
+    ListaConversaReducer
 });
--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {View, Text, ListView} from "react-native";
+import {View, Text, ListView, TouchableHighlight, Touchable} from "react-native";
 import { connect } from 'react-redux';
 import _ from "lodash";
 import {contatosUsuarioFetch} from '../Actions/AppActions';
+import { Actions } from 'react-native-router-flux';
 
 class Contatos extends Component {
     
@@ -21,21 +22,27 @@ class Contatos extends Component {
         this.fonteDeDados = ds.cloneWithRows(contatos);
     }
 
+    renderRow(contato){
+        return(
+            <TouchableHighlight onPress={() => Actions.conversa({title: contato.nome, contatoNome: contato.nome, contatoEmail: contato.email})} underlayColor="#CCC">
+                <View style={{flex: 1, padding: 20, borderBottomWidth: 2, borderColor: '#CCC'}}>
+                    <Text style={{fontSize: 25}}>
+                        {contato.nome}
+                    </Text>
+                    <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                        {contato.email}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+
     render(){
         return(
             <ListView
                 enableEmptySections
                 dataSource={this.fonteDeDados}
-                renderRow={data => (
-                    <View style={{flex: 1, padding: 20, borderBottomWidth: 2, borderColor: '#CCC'}}>
-                        <Text style={{fontSize: 25}}>
-                            {data.nome}
-                        </Text>
-                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-                            {data.email}
-                        </Text>
-                    </View>
-                )}
+                renderRow={data => (this.renderRow(data))}
             />
         )
     }
