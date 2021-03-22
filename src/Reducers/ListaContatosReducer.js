f9858563fae83b68b3ad482d35122dbcc6fb897d
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-22 08:10:51 -0300

    Aba de lista contatos pronto

---
+++ src/Reducers/ListaContatosReducer.js
@@ -0,0 +1,12 @@
+import {LISTA_CONTATO_USUARIO} from "../Actions/types";
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+    console.log('teste', action.payload);
+    switch (action.type) {
+        case LISTA_CONTATO_USUARIO:
+            return action.payload;
+        default: 
+            return state;
+    }
+}
--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React, {Component} from 'react';
+import {View, Text, ListView} from "react-native";
+import { connect } from 'react-redux';
+import _ from "lodash";
+import {contatosUsuarioFetch} from '../Actions/AppActions';
 
-export default props => (
-    <View>
-        <Text>Contatos</Text>
-    </View>
-)
+class Contatos extends Component {
+    
+    componentWillMount() {
+        this.props.contatosUsuarioFetch();
+        this.criaFonteDeDados( this.props.contatos );
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.criaFonteDeDados(nextProps.contatos)
+    }
+
+    criaFonteDeDados( contatos ){
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+        this.fonteDeDados = ds.cloneWithRows(contatos);
+    }
+
+    render(){
+        return(
+            <ListView
+                enableEmptySections
+                dataSource={this.fonteDeDados}
+                renderRow={data => (
+                    <View style={{flex: 1, padding: 20, borderBottomWidth: 2, borderColor: '#CCC'}}>
+                        <Text style={{fontSize: 25}}>
+                            {data.nome}
+                        </Text>
+                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                            {data.email}
+                        </Text>
+                    </View>
+                )}
+            />
+        )
+    }
+}
+mapStateToProps = state => {
+    const contatos = _.map(state.ListaContatosReducer, (val, uid) => {
+        return {...val, uid}
+    })
+    return {
+        contatos
+    }
+}
+export default connect(mapStateToProps, { contatosUsuarioFetch })(Contatos)
